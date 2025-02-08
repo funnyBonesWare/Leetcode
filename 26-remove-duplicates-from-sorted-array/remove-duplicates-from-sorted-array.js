e859626345d9d4a9d6095dff48e1bf6cc2b23585
funnyBonesWare <sahilsiddiquihere@gmail.com>
2025-02-08 17:23:50 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (50.69%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0; // Edge case: empty array
+
+    let k = 0; // Slow pointer to track unique elements
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k]) { // Found a new unique element
+            k++; // Move to the next position
+            nums[k] = nums[i]; // Place the unique element at new position
+        }
+    }
+    return k + 1; // Number of unique elements
+};
